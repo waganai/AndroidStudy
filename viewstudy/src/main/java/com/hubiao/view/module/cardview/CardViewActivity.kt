2322097c1792aa5f95661c4e9f7c8a987283b4c0
胡标 <biao.hu@yintech.cn>
2020-12-29 18:20:15 +0800

    [功能][CardView]添加CardView显示功能

--- viewstudy/src/main/java/com/hubiao/view/module/cardview/CardViewActivity.kt
+++ viewstudy/src/main/java/com/hubiao/view/module/cardview/CardViewActivity.kt
@@ -1,14 +1,38 @@
 package com.hubiao.view.module.cardview
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hubiao.view.R
 
-class CardViewActivity: Activity() {
+class CardViewActivity : AppCompatActivity() {
+
+    var mCardViewFragment: CardViewFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_cardview_layout)
+
+        init()
+    }
+
+    private fun init() {
+        initView()
+
+        initListener()
+    }
+
+    private fun initView() {
+        mCardViewFragment = CardViewFragment.buildFragment()
+
+        mCardViewFragment?.let {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fl_container, it, "")
+            transaction.commit()
+        }
+    }
+
+    private fun initListener() {
+
     }
 }
--- viewstudy/src/main/java/com/hubiao/view/module/cardview/CardViewFragment.kt
+++ viewstudy/src/main/java/com/hubiao/view/module/cardview/CardViewFragment.kt
@@ -1,6 +1,29 @@
 package com.hubiao.view.module.cardview
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hubiao.view.R
 
-class CardViewFragment: Fragment() {
+class CardViewFragment : Fragment() {
+
+    companion object {
+        fun buildFragment(): CardViewFragment {
+            return CardViewFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cardview_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
