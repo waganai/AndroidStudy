694e74f1a040994c9c2284f88ffd5b729e511640
胡标 <biao.hu@yintech.cn>
2020-12-29 20:06:50 +0800

    [功能][DoKit]集成DoKit

--- viewstudy/src/main/java/com/hubiao/view/SimpleApplication.kt
+++ viewstudy/src/main/java/com/hubiao/view/SimpleApplication.kt
@@ -1,6 +1,17 @@
 package com.hubiao.view
 
 import android.app.Application
+import com.didichuxing.doraemonkit.DoraemonKit
+import com.didichuxing.doraemonkit.kit.AbstractKit
 
 class SimpleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DoraemonKit.install(SimpleApplication@this,
+            LinkedHashMap<String, MutableList<AbstractKit>>(),
+            "pId");
+    }
+
 }
