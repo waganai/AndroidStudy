d03bc50f976bb75e7cb25c9a8b31999bd7527847
胡标 <biao.hu@yintech.cn>
2021-01-10 12:07:37 +0800

    [功能][JetPack]Fragment使用LiveData功能

--- jetpackdemo/src/main/java/com/example/jetpackdemeo/CounterModel.kt
+++ jetpackdemo/src/main/java/com/example/jetpackdemeo/CounterModel.kt
@@ -1,29 +1,32 @@
 package com.example.jetpackdemeo
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class CounterModel(counterReversed: Int) : ViewModel() {
-    var counter = MutableLiveData<Int>()
+    val counter: LiveData<Int>
+        get() = _counter
+    private var _counter = MutableLiveData<Int>()
 
     init {
-        counter.value = counterReversed
+        _counter.value = counterReversed
     }
 
     fun plusOne() {
-        val value = counter.value ?: 0
+        val value = _counter.value ?: 0
         // 此处不能直接获取counter.value ?: 0加上1赋值，否则额没有变化，原因未知
-        counter.postValue(value + 1)
+        _counter.postValue(value + 1)
     }
 
     fun reduceOne() {
-        val value = counter.value ?: 0
-        counter.postValue(value - 1)
+        val value = _counter.value ?: 0
+        _counter.postValue(value - 1)
     }
 
     fun clear() {
-        counter.postValue(0)
+        _counter.postValue(0)
     }
 
     class CounterModelFactory(private val counterReversed: Int) : ViewModelProvider.Factory {
