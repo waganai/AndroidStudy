bf2d9d64f27d7c32b18df932b4e3494f42c338df
waganai <358973601@qq.com>
2021-03-10 10:19:59 +0800

    [功能][第三方库]Retrofit

---
+++ jetpackdemo/src/main/java/com/example/jetpackdemeo/thirdframework/retrofit/RetrofitTestActivity.kt
@@ -0,0 +1,18 @@
+package com.example.jetpackdemeo.thirdframework.retrofit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RetrofitTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+
+    }
+
+}
