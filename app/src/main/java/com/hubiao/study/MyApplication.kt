c6c6b6d9419cf8fa293d85711d6b706ebc7a6296
waganai <358973601@qq.com>
2021-03-19 11:29:10 +0800

    [功能][第三方库]ARouter 路由

--- app/src/main/java/com/hubiao/study/MyApplication.kt
+++ app/src/main/java/com/hubiao/study/MyApplication.kt
@@ -14,9 +14,13 @@ class MyApplication: BaseApplication() {
     override fun onCreate() {
         super.onCreate()
 
-        if (Config.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+        if (BuildConfig.DEBUG) {
+            Log.e(TAG, "onCreate() if (Config.DEBUG)")
+            // 这两行必须写在init之前，否则这些配置在init过程中将无效
             ARouter.openLog();     // 打印日志
             ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        } else {
+            Log.e(TAG, "onCreate() not Config.DEBUG)")
         }
 
         Log.e(TAG, "onCreate() ARouter.init(this) start")
