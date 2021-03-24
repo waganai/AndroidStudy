af3f2cf74b92129e0c8d792cb10acc524f21e2bd
waganai <358973601@qq.com>
2021-03-24 12:34:08 +0800

    [功能][RxJava]lift操作符

--- jetpackdemo/src/main/java/com/example/jetpackdemeo/thirdframework/rxjava/RxJavaOperatorTestActivity.kt
+++ jetpackdemo/src/main/java/com/example/jetpackdemeo/thirdframework/rxjava/RxJavaOperatorTestActivity.kt
@@ -2,10 +2,19 @@ package com.example.jetpackdemeo.thirdframework.rxjava
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jetpackdemeo.databinding.ActivityRxjavaOperatorLayoutBinding
+import io.reactivex.Observable
+import io.reactivex.ObservableOperator
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 
 class RxJavaOperatorTestActivity : AppCompatActivity() {
+
+    companion object{
+        val TAG = RxJavaFunctionOperatorTestActivity::class.simpleName
+    }
 
     var viewBinding: ActivityRxjavaOperatorLayoutBinding? = null
 
@@ -49,11 +58,49 @@ class RxJavaOperatorTestActivity : AppCompatActivity() {
             }
 
             btnFunction.setOnClickListener {
-                startActivity(Intent(this@RxJavaOperatorTestActivity,
-                RxJavaFunctionOperatorTestActivity::class.java))
+                startActivity(
+                    Intent(
+                        this@RxJavaOperatorTestActivity,
+                        RxJavaFunctionOperatorTestActivity::class.java
+                    )
+                )
             }
 
             btnBool.setOnClickListener { }
+
+            btnLift.setOnClickListener {
+                Observable.just(1, 2, 3, 4, 5)
+                    .lift(ObservableOperator<String, Int> { observer ->
+                        object: Observer<Int>{
+                            override fun onSubscribe(d: Disposable) {
+                            }
+
+                            override fun onNext(t: Int) {
+                                observer.onNext("lift 操作符 $t")
+                            }
+
+                            override fun onError(e: Throwable) {
+                            }
+
+                            override fun onComplete() {
+                            }
+                        }
+                    })
+                    .subscribe(object:Observer<String>{
+                        override fun onSubscribe(d: Disposable) {
+                        }
+
+                        override fun onNext(t: String) {
+                            Log.e(TAG, "lift() onNext($t)")
+                        }
+
+                        override fun onError(e: Throwable) {
+                        }
+
+                        override fun onComplete() {
+                        }
+                    })
+            }
         }
     }
 }
