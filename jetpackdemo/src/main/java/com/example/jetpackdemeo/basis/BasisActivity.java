abf3f6dcfa715357e1530e72c651f04ee1e72aa9
waganai <358973601@qq.com>
2021-04-12 12:44:24 +0800

    [功能][集合]HashMap  ConcurrentHashMap

--- jetpackdemo/src/main/java/com/example/jetpackdemeo/basis/BasisActivity.java
+++ jetpackdemo/src/main/java/com/example/jetpackdemeo/basis/BasisActivity.java
@@ -7,6 +7,9 @@ import android.util.SparseArray;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class BasisActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +20,11 @@ public class BasisActivity extends AppCompatActivity {
     }
 
     private void init() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put(null, null);
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put(null, null);
+
         SparseArray<String> sparseArray = new SparseArray<>();
         ArrayMap<String, String> arrayMap = new ArrayMap<>();
     }
