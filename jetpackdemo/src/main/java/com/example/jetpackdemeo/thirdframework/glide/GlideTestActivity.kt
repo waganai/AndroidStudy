a98d9864fa5a41d55d6dfbb61132b8a0d2075012
waganai <358973601@qq.com>
2021-04-26 10:32:46 +0800

    [三方框架][Glide]RequestOption

--- jetpackdemo/src/main/java/com/example/jetpackdemeo/thirdframework/glide/GlideTestActivity.kt
+++ jetpackdemo/src/main/java/com/example/jetpackdemeo/thirdframework/glide/GlideTestActivity.kt
@@ -2,8 +2,14 @@ package com.example.jetpackdemeo.thirdframework.glide
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.GenericTransitionOptions
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.example.jetpackdemeo.databinding.ActivityGlideTestLayoutBinding
+import com.hubiao.base.R
 
 class GlideTestActivity : AppCompatActivity() {
 
@@ -24,6 +30,11 @@ class GlideTestActivity : AppCompatActivity() {
             Glide
                 .with(this@GlideTestActivity)
                 .load("https://b0.bdstatic.com/comment/AbD06ZlrNmmzmXl7Cw5uFA957ac5114bcf7a68d6bd51e8493dfded.jpg@w_967,h_967")
+                .apply(RequestOptions().circleCrop())
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.RESOURCE))
+                .apply(RequestOptions.skipMemoryCacheOf(true))
+                .apply(RequestOptions.placeholderOf(R.mipmap.ic_launcher))
+                .transition(GenericTransitionOptions.withNoTransition())
                 .into(ivGlide1)
         }
     }
